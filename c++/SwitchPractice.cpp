62a2710c5033b62f715712c55b4f9d97fd1118e6
uswaminael-dev <uswaminahil.dev@gmail.com>
2025-07-31 22:43:06 +0500

    Switch Practice

---
+++ c++/SwitchPractice.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char initial;
+    cout<<"WELCOME TO THE HOPE CODE"<<endl<<"Please Enter The Initial Of Your Name To Get Your Hope"<<endl;
+    cin>>initial;
+
+    switch (initial){
+    case 'a':
+        cout<<"Allah's mercy is greater than all the sins,nothing can exceed His Mercy not even your sins";
+        break;
+    case 'b':
+        cout<<"With every hardship comes the ease , then how can you think that you will not get the releif";
+        break;
+    case 'c':
+        cout<<"Allah doesn't burden a soul beyong capacity so come on champ you are that strong";
+        break;
+    case 'd':
+        cout<<"Allah is with the patient, He saw you controlling your self and bearing patience, remain hopeful for your reward";
+        break;
+    case 'e':
+        cout<<"Turn to Allah , He is closer to you than your jugular vein, I hope you will not feel lonely now";
+        break;
+    case 'f':
+        cout<<"After fear and sorrow comes safety and joy, so don't lose hope and keep your trust in Allah";
+        break;
+    case 'g':
+        cout<<"Allah never forgets your efforts and never waste them, you will be rewarded more than your efforts soon";
+        break;
+    case 'h':
+        cout<<"Juat have tawakkul, for Our Rab says whoever trusts Me I become sufficient for him";
+        break;
+    case 'i':
+        cout<<"He nows what you are going through and He knows every detail of your pain, you don't even have to tell Him, so flee to Him and ask Him to mend your heart";
+        break;
+    case 'j':
+        cout<<"Your Lord has not forsaken you nor has He forgotten";
+        break;
+    case 'k':
+        cout<<"And it maybe that you dislike anything while it is good for you, who knows except Allah";
+        break;
+    case 'l':
+        cout<<"Be grateful dear soul for when you are grateful Allah increases you in His blessings, so be grateful for what you have and what you don't have";
+        break;
+    case 'm':
+        cout<<"Until when you think all hope is lost, Allah will send you a way out, so don't lose hope and keep praying";
+        break;
+    case 'n':
+        cout<<"so remember Allah, He will remember you, and be grateful to Him and do not deny Him";
+        break;
+    case 'o':
+        cout<<"My lord is most Merciful and He is the best of those who show mercy, so don't lose hope in His mercy";
+        break;
+    case 'p':
+        cout<<"No soul knows what it will earn tomorrow, so don't worry about the future, just do your best today and leave the rest to Allah";
+        break;
+    case 'q':
+        cout<<"Light up your heart with the remembrance of Allah, for verily in the remembrance of Allah do hearts find rest";
+        break;
+    case 'r':
+        cout<<"Allah is the best of planners, so trust His plan for you, it is better than your own plan";
+        break;
+    case 's':
+        cout<<"Do not despair of the mercy of Allah, for verily no one despairs of the mercy of Allah except the disbelieving people";
+        break;
+    case 't':
+        cout<<"Allah loves those who turn to Him in repentance, so repent and turn back to Him, for He is the Most Merciful";
+        break;
+    case 'u':
+        cout<<"And your Lord is going to give you, and you will be satisfied, so keep your faith strong and trust in His promises";
+        break;
+    case 'v':
+        cout<<"Test your patience, for Allah is with the patient, and He will reward you for your endurance";
+        break;
+    case 'w':
+        cout<<"Remain steadfast in your prayers and be patient, for verily Allah is with the patient";
+        break;
+    case 'x':
+        cout<<"Nothing can happen to us except what Allah has decreed for us, so trust His wisdom and plan";
+        break;
+    case 'y':
+        cout<<"Allah is with you, so do not grieve, for verily He is the best of helpers";
+        break;
+    case 'z':
+        cout<<"Certainly the help of Allah is near, so be patient and keep your faith strong, for surely He will come to your aid";
+        break;    
+    default:
+        cout<<"OOPS!Maybe you entered something else:()";
+        break;
+    }
+
+}
--- c++/basics.cpp
+++ c++/basics.cpp
@@ -1,71 +1,11 @@
-
 #include <iostream>
-#include <string>
 using namespace std;
 
-// Function example
-int add(int a, int b) {
-    return a + b;
-}
+int main(){
+    
+    char initial;
+    cout<"WELCOME TO THE HOPE CODE"<<endl<<"Please write the initial of your name to get to know what message is there for you";
+    cin>>initial;
 
-// Simple class example
-class Person {
-public:
-    string name;
-    int age;
-    void introduce() {
-        cout << "Hi, my name is " << name << " and I am " << age << " years old." << endl;
-    }
-};
-
-int main() {
-    // Variables
-    int x = 5;
-    double y = 3.14;
-    char c = 'A';
-    string s = "sk";
-
-    // Output
-    cout << "x = " << x << ", y = " << y << ", c = " << c << ", s = " << s << endl;
-
-    // Input
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << "You entered: " << num << endl;
-
-    // Conditionals
-    if (num > 0) {
-        cout << "Positive number" << endl;
-    } else if (num < 0) {
-        cout << "Negative number" << endl;
-    } else {
-        cout << "Zero" << endl;
-    }
-
-    // Loops
-    cout << "Counting from 1 to 5:" << endl;
-    for (int i = 1; i <= 5; ++i) {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    // Arrays
-    int arr[3] = {10, 20, 30};
-    cout << "Array elements: ";
-    for (int i = 0; i < 3; ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    // Function call
-    cout << "Sum of 2 and 3 is: " << add(2, 3) << endl;
-
-    // Class usage
-    Person p;
-    p.name = "Uswa";
-    p.age =19;
-    p.introduce();
-
-    return 0;
+    
 }
