f65dbd578352321b2190e06dad216849dedfc5b8
uswaminael-dev <uswaminahil.dev@gmail.com>
2025-08-08 11:44:14 +0500

    getting a number from user and printing it's factorial

--- c++/Functions.cpp
+++ c++/Functions.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int factorial(int num1){
     int product=1;
-    for(int i=1,i<=num1,i++){
+    for(int i=1;i<=num1;i++){
         product=product*i;
 }
-    cout<<"The factorial of"<<num1<< "is"<<product<<endl;
+    cout<<"The factorial of "<<num1<< " is "<<product<<endl;
 }
 
 int main(){
-    factorial(5);
+    int num;
+    cout<<"Enter a number to find its factorial: ";
+    cin>>num;
+    factorial(num);
     return 0;
 }
