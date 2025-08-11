e6683bb1c3c1e3fab43a55fbf7aaf414f2eb4720
uswaminael-dev <uswaminahil.dev@gmail.com>
2025-08-11 23:04:19 +0500

    Python Crash Course by NUST

---
+++ python/PCC.01.py
@@ -0,0 +1,17 @@
+#INSTALLATION OF PYTHON ON GOOGLE COLAB
+
+print("HELLO WORLD")
+print("Happy Coding")
+
+#VARIABLE INITIALIZATION AND DECLARATION
+var_1=47
+var_2=0.56
+var_3="Hello"
+
+#PRINTNG A VARIABLE
+print(var_3)
+
+#Strings don't accept any mathematical operation and are always written in quotes
+
+#= ----> Assignment operator
+
