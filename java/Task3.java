12bbba48d64ef912f9ccc1a7ad69850f681dc418
uswaminael-dev <uswaminahil.dev@gmail.com>
2025-09-17 23:32:29 +0500

    java practice

--- java/Task3.java
+++ java/Task3.java
@@ -2,14 +2,14 @@ import java.util.*;
 public class Task3{
     static Scanner sc = new Scanner (System.in);
     public static void main (String [] args){
-        System.out.println("Enter number of students in class A: ");
+        System.out.print("Enter number of students in class A: ");
         int classA = sc.nextInt();
-        System.out.println("Enter number of students in class B: ");
+        System.out.print("Enter number of students in class B: ");
         int classB = sc.nextInt();
-        System.out.println("Enter number of students in class C: ");
+        System.out.print("Enter number of students in class C: ");
         int classC = sc.nextInt();
         int totalStudents = classA + classB + classC;
         int desksNeeded = (totalStudents ) / 2; 
-        System.out.println("Total number of desks needed: " + desksNeeded);
+        System.out.print("Total number of desks needed: " + desksNeeded);
     }
 }
